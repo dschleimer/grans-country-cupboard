5cb1b345aa96eb4c27e2b46874497514c1d6e15f
David Schleimer <dschleimer@gmail.com>
2025-07-22 12:21:54 -0700

    [ingredients] generate collection with placeholder content

--- _plugins/ingredients.rb
+++ _plugins/ingredients.rb
@@ -2,6 +2,7 @@ require 'set'
 
 require "jekyll"
 require "jekyll/filters"
+require "jekyll/document"
 
 module Ingredients
     class IngredientsGenerator < Jekyll::Generator 
@@ -9,6 +10,11 @@ module Ingredients
 
         def generate(site)
             book_recipes = site.collections["book_recipes"].docs
+            all_ingredients = extract_tag_and_link_recipes(book_recipes)
+            generate_ingredient_collection(site, all_ingredients)
+        end
+
+        def extract_tag_and_link_recipes(book_recipes)
             all_ingredients = Set[]
             for recipe in book_recipes do
                 new_content = []
@@ -35,11 +41,28 @@ module Ingredients
                     cells[1] = " [#{ingredient}](/ingredients/#{slug}.html) "
                     new_content.append(cells.join('|'))
                 end
-                tags = recipe_ingredients.map!{|i| "__ingredient:#{i}"}.to_a
+                tags = recipe_ingredients.map {|i| "__ingredient:#{i}"}.to_a
                 recipe.data["tags"] = recipe.data.fetch("tags", []) + tags
                 recipe.content = new_content.join()
+                all_ingredients |= recipe_ingredients
             end
+            all_ingredients
         end
 
+        def generate_ingredient_collection(site, all_ingredients)
+            collection = site.collections["ingredients"]
+            for ingredient in all_ingredients.to_a.sort do
+                slug = Jekyll::Utils.slugify(ingredient)
+                path = File.join(site.source, '_ingredients', "#{slug}.md")
+                doc = Jekyll::Document.new(path, {:site => site, :collection => collection})
+                doc.merge_data!({
+                    "title" => ingredient.split().map {|w| w.capitalize}.join(" "),
+                    "ingredient" => ingredient,
+                    "layout" => "default"
+                })
+                doc.content = '{% include ingredient.md %}'
+                collection.docs << doc
+            end
+        end
     end
 end
